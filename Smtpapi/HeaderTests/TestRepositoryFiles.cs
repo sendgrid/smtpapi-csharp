4ef0013b54ca2da8f3410745643734e47bb9515b
Dawid Treściński <blitzerpl@gmail.com>
2017-10-29 08:57:36 +0100

    Added TestRepositoryFiles.cs

---
+++ Smtpapi/HeaderTests/TestRepositoryFiles.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace SendGrid.SmtpApi.HeaderTests
+{
+    [TestFixture]
+    public class TestRepositoryFiles
+    {
+        [Test]
+        public void TestLicenseEndYear()
+        {
+            string[] pathsArray = new string[] { "..", "..", "..", "..", "LICENSE.txt" };
+            string licensePath = Path.Combine(pathsArray);
+
+            string line = File.ReadLines(licensePath).First();
+
+            Assert.AreEqual(DateTime.Now.Year.ToString(), line.Substring(19, 4));
+        }
+    }
+}
